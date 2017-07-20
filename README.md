ee9f571f429d0a8f9a54b256eae27a9408da7bf8
hisener <hisener@yahoo.com>
2017-07-20 17:15:35 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# opsgenie-notifier
+# OpsGenie Notifier
+
 Experimental OpsGenie Alert notifications on native desktop.
+
+[![Build Status][travis-image]][travis-url]
+[![Code Climate](https://codeclimate.com/github/hisener/opsgenie-notifier/badges/gpa.svg)](https://codeclimate.com/github/hisener/opsgenie-notifier)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+[travis-image]: https://travis-ci.org/hisener/opsgenie-notifier.svg?branch=master
+[travis-url]: https://travis-ci.org/hisener/opsgenie-notifier
+
+![Demo Gif](demo.gif)
+
+## Requirements
+- Node.js 6+
+
+## Install and start
+```bash
+git clone https://github.com/hisener/opsgenie-notifier.git
+cd opsgenie-notifier
+npm install
+# Set your API key in .env
+npm start
+```
+
+## Known issues
+- Notifications disapper after 5s and it cannot changeable. mikaelbr/node-notifier/#172
+
+## Credits
+- [node-notifier](https://github.com/mikaelbr/node-notifier) for sending notifications
+- [opsgenie-sdk](https://github.com/opsgenie/opsgenie-nodejs-sdk)
+
+## TODO
+- [x] Demo gif/photo
+- [ ] package.json bin
+
+## License
+
+[MIT. Copyright (C)](LICENCE) [Halil İbrahim Şener](http://halilsener.com).
---
+++ index.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const opsgenie = require('opsgenie-sdk')
+const notifier = require('node-notifier')
+
+require('dotenv').config()
+
+opsgenie.configure({
+  'host': process.env.OG_HOST,
+  'api_key': process.env.OG_API_KEY
+})
+
+let actions = [
+  {
+    name: 'Acknowledge Alert',
+    callback: function (alertId) {
+      opsgenie.alertV2.acknowledge({ identifier: alertId }, {}, console.log)
+    }
+  },
+  {
+    name: 'Close Alert',
+    callback: function (alertId) {
+      opsgenie.alertV2.close({ identifier: alertId }, {}, console.log)
+    }
+  }
+]
+
+let knownAlerts = []
+
+setInterval(function () {
+  getOpenAlerts()
+    .then(function (alerts) {
+      alerts.forEach(function (alert) {
+        if (!knownAlerts.includes(alert.id)) {
+          console.log(new Date().toISOString(), 'New Alert', alert.id)
+          knownAlerts.push(alert.id)
+          notify(alert)
+        }
+      })
+    })
+    .catch(function (err) {
+      console.error(new Date().toISOString(), err)
+    })
+}, process.env.INTERVAL || 15000)
+
+function getOpenAlerts () {
+  return new Promise(function (resolve, reject) {
+    opsgenie.alertV2.list({
+      query: 'status: open', // TODO: AND createdAt Date.now()
+      limit: 3
+    }, function (err, res) {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(JSON.parse(res).data)
+    })
+  })
+}
+
+function notify (alert) {
+  notifier.notify({
+    title: 'New Alert',
+    message: alert.message,
+    icon: path.join(__dirname, 'opsgenie-logo.png'),
+    wait: true,
+    closeLabel: 'Ignore',
+    actions: actions.map(action => action.name),
+    alertId: alert.id
+  }, function (err, response, metadata) {
+    if (err) {
+      console.error(err)
+    }
+  })
+}
+
+notifier.on('click', function (notifierObject, options, metadata) {
+  actions[metadata.activationValueIndex].callback(options.alertId)
+})
